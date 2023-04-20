994629600f774350e93fc4da60cc116a57c92fd5
uniqcle <uniqcle@yandex.ru>
2023-04-20 12:30:34 +0700

    mobile nav

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const mobileNavBtn = document.querySelector('.mobile-nav-btn');
+const sideBar = document.querySelector('.sidebar');
+
+mobileNavBtn.addEventListener('click', function () {
+    // console.log(sideBar);
+    sideBar.classList.toggle('sidebar--active');
+
+})
+
+console.log(mobileNavBtn); 
