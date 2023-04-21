6627627d462c6caec1b85a8ddd6479432a1e6802
uniqcle <uniqcle@yandex.ru>
2023-04-21 11:09:31 +0700

    checkbox

--- js/main.js
+++ js/main.js
@@ -7,21 +7,61 @@ mobileNavBtn.addEventListener('click', function () {
 
 })
 
-/* показать еще 3 карточки */ 
+/* показать еще 3 карточки */
 const btnMore = document.querySelector('.btn-more');
 const hiddenCards = document.querySelectorAll('.cards-link--hidden');
-
-console.log(hiddenCards);
 
 btnMore.addEventListener('click', function () {
 
     hiddenCards.forEach(card => {
-        console.log(card);
-
         card.classList.remove('cards-link--hidden');
     })
 
 })
 
 
-/* показать/скрыть контент виджеты */ 
+/* показать/скрыть контент виджеты */
+const widgetsTitle = document.querySelectorAll('.widget__title');
+
+widgetsTitle.forEach(title => {
+
+    title.addEventListener('click', function () {
+        title.nextElementSibling.classList.toggle('none')
+        title.classList.toggle('widget__title--active')
+    })
+})
+
+/* сброс инпутов по кнопке любая */
+const checkboxAny = document.querySelector('#location-05');
+const allLocationCheckboxes = document.querySelectorAll('.location__checkbox');
+
+checkboxAny.addEventListener('change', function () {
+
+    if (checkboxAny.checked) {
+        allLocationCheckboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        })
+        checkboxAny.checked = true;
+    }
+})
+
+
+allLocationCheckboxes.forEach(checkbox => {
+    if (checkbox !== checkboxAny) {
+        checkbox.addEventListener('change', function () {
+            checkboxAny.checked = false;
+        })
+    }
+})
+
+
+/* Показать еще */
+const optionsShowMore = document.querySelector('.options-show-more')
+const hiddenElementsWidgets = document.querySelector('.widget__elements--hidden');
+
+optionsShowMore.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    hiddenElementsWidgets.classList.remove('widget__elements--hidden')
+    optionsShowMore.remove();
+})
